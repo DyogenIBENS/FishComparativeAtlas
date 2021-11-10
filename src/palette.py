02aef1f0c898962c99bb87ce7af4fbefcd019573
eparey <parey@biologie.ens.fr>
2021-11-10 13:28:46 +0100

    tracking files

---
+++ src/palette.py
@@ -0,0 +1,17 @@
+PALETTE = {'1a': "lime", '1b': "greenyellow", "13a": "red", "13b":"crimson",\
+           "8b":"darkorange", "8a": "orangered", "6a":'#CD00CD', '6b': "#FF32FF",\
+           "9a":"darkblue", "9b":"royalblue", "12a":"black", "12b":"#404040",\
+           "2a": "darkgreen", "2b": "mediumseagreen", "3a":"#707070", "3b":'#B8B8B8',\
+           '5a': "brown", "5b": "peru", "4a": "gold", "4b":"yellow", "7a":"mediumorchid",
+           "7b": "plum", "11a":"deeppink", "11b":"hotpink", "10a": "deepskyblue",\
+           "10b": "lightskyblue"}
+
+# PALETTE_html = {'5a': "6a3d9a", '5b': "6a3d9a", "1a": "6a3d9a", "1b":"6a3d9a",\
+#            "9b":"6a3d9a", "9a": "6a3d9a", "13a":'6a3d9a', '13b': "6a3d9a",\
+#            "3a":"6a3d9a", "3b":"6a3d9a", "2a":"6a3d9a", "2b":"6a3d9a",\
+#            "8a": "6a3d9a", "8b": "6a3d9a", "6a":"6a3d9a", "6b":'6a3d9a',\
+#            '4a': "6a3d9a", "4b": "6a3d9a", "10a": "6a3d9a", "10b":"6a3d9a", "11a":"6a3d9a",
+#            "11b": "6a3d9a", "12a":"6a3d9a", "12b":"6a3d9a", "7a": "6a3d9a",\
+#            "7b": "6a3d9a"}
+
+# REORDER_CHROMS = {1:13, 2:12, 3:9, 4:5, 5:1, 6:3, 7:10, 8:2, 9:8, 10:4, 11:7, 12:11, 13:6}
